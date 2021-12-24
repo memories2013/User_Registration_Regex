ffd505d1c45bf0d6e1ee5a090e69ad6f5fcc8c60
memories2013 <nshivaprasad41@gmail.com>
2021-12-24 08:57:01 +0530

    REFACTOR program to accept the full name

--- src/com/user/RegexTest.java
+++ src/com/user/RegexTest.java
@@ -7,7 +7,7 @@ public class RegexTest {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		System.out.print(" Please enter the string: ");
+		System.out.print(" Please enter the full name: ");
 		String name = scan.nextLine();
 		
 		UserDetails userDetails = new UserDetails();
